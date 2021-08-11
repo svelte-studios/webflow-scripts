8826ea0842fb9d5048be9e8b73b32978f956f4a4
Terence Kruger <terencejkruger@gmail.com>
2021-08-11 21:48:03 +0930

    Init with filter script

---
+++ readme.md
@@ -0,0 +1,4 @@
+1. "filter-key" + "filter-all"
+2. "filter-items"
+3. "filter-item"
+4. "filter-value"
---
+++ src/filter.js
@@ -0,0 +1,43 @@
+const filterAllClass = `filter-all`;
+const filterKeySelector = `.filter-key`;
+const filterItemsSelector = `.filter-items`;
+const filterItemSelector = `.filter-item`;
+const filterValueSelector = `.filter-value`;
+
+const convToFilter = function (str) {
+  if (!str) {
+    str = "empty";
+  }
+  return str
+    .replace(/[!\"#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, "")
+    .replace(/ /g, "-")
+    .toLowerCase()
+    .trim();
+};
+
+const containerEl = document.querySelector(filterItemsSelector);
+
+$(filterKeySelector).each(function () {
+  const filterKeyText = "." + convToFilter($(this).text());
+  if ($(this).hasClass(filterAllClass)) filterKeyText = "all";
+  $(this).attr("data-filter", filterKeyText);
+});
+
+$(filterValueSelector).each(function () {
+  const filterKeyText = convToFilter($(this).text());
+  $(this).closest(filterItemSelector).addClass(filterKeyText);
+});
+
+const initialFilter = function () {
+  if (url("?filter")) return "." + url("?filter");
+  return "all";
+};
+
+mixitup(containerEl, {
+  selectors: {
+    target: filterItemSelector,
+  },
+  load: {
+    filter: initialFilter(),
+  },
+});
