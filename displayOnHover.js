0099ef924f3bf6cf1f2ae1789922627732aca3e1
JakeVandborg <jake.vandborg@gmail.com>
2021-08-31 03:17:45 +0000

    Deploying to gh-pages from @ svelte-studios/webflow-scripts@96bc3c0b254b2c5af9d9cdf483b5535e3d826f90 🚀

--- displayOnHover.js
+++ displayOnHover.js
@@ -2,7 +2,11 @@
 
 var featureListItemSelector = ".featurelistitem";
 var featureListContent = ".featurelistcontent";
-var contentContainerEl = document.querySelector(featureListContent);
+var featureListContainerSelector = ".featurelistcontainer";
+var activeItemClass = ".active";
+$(featureListContent).children().eq(0).css({
+  display: "block"
+});
 $(featureListItemSelector).mouseenter(function () {
   var listItemIndex = $(this).index();
   $(featureListContent).children().each(function () {
@@ -10,9 +14,11 @@ $(featureListItemSelector).mouseenter(function () {
     if ($(this).css("display") !== "none") $(this).css({
       display: "none"
     });
+    if ($(this).hasClass(activeItemClass)) $(this).removeClass(activeItemClass);
   });
   console.log("🚀 ~ file: displayOnHover.js ~ line 20 ~ $(featureListContent).children().eq(listItemIndex).index()", $(featureListContent).children().eq(listItemIndex).index());
   $(featureListContent).children().eq(listItemIndex).css({
     display: "block"
   });
+  $(featureListContainerSelector).children().eq(listItemIndex).addClass(activeItemClass);
 });
