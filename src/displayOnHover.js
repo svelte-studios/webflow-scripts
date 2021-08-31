aaf82727a858441235bc6a96ee128b6ca3e6316d
Jake Vandborg <jake.vandborg@gmail.com>
2021-08-31 12:52:53 +0930

    debugging

--- src/displayOnHover.js
+++ src/displayOnHover.js
@@ -11,19 +11,17 @@ $(featureListItemSelector).mouseenter(function () {
   $(featureListContent)
     .children()
     .each(function () {
-      console.log(
-        "🚀 ~ file: displayOnHover.js ~ line 17 ~ this.index()",
-        $(this).index()
-      );
       if ($(this).css("display") !== "none") $(this).css({ display: "none" });
-      if ($(this).hasClass(activeItemClass))
+      if ($(this).hasClass(activeItemClass)) {
+        console.log(
+          "🚀 ~ file: displayOnHover.js ~ line 20 ~ $(this).hasClass(activeItemClass)",
+          $(this).hasClass(activeItemClass)
+        );
+
         $(this).removeClass(activeItemClass);
+      }
     });
 
-  console.log(
-    "🚀 ~ file: displayOnHover.js ~ line 20 ~ $(featureListContent).children().eq(listItemIndex).index()",
-    $(featureListContent).children().eq(listItemIndex).index()
-  );
   $(featureListContent).children().eq(listItemIndex).css({ display: "block" });
   $(featureListContainerSelector)
     .children()
