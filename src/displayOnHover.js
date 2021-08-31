d29781556c038400d4be0d165cb4051d72a6308e
Jake Vandborg <jake.vandborg@gmail.com>
2021-08-31 13:06:54 +0930

    better named variables

--- src/displayOnHover.js
+++ src/displayOnHover.js
@@ -1,35 +1,29 @@
 const featureListItemSelector = ".featurelistitem";
-const featureListContent = ".featurelistcontent";
-const featureListContainerSelector = ".featurelistcontainer";
+const featuredContentSelector = ".featuredcontent";
+const featureListSelector = ".featurelist";
 const activeItemClass = "active";
 
-$(featureListContent).children().eq(0).css({ display: "block" });
+$(featuredContentSelector).children().eq(0).css({ display: "block" });
 
 $(featureListItemSelector).mouseenter(function () {
   const listItemIndex = $(this).index();
 
-  $(featureListContent)
+  $(featuredContentSelector)
     .children()
     .each(function () {
       if ($(this).css("display") !== "none") $(this).css({ display: "none" });
     });
 
-  $(featureListContainerSelector)
+  $(featureListSelector)
     .children()
     .each(function () {
-      if ($(this).hasClass(activeItemClass)) {
-        console.log(
-          "🚀 ~ file: displayOnHover.js ~ line 20 ~ $(this).hasClass(activeItemClass)",
-          $(this).hasClass(activeItemClass)
-        );
-
+      if ($(this).hasClass(activeItemClass))
         $(this).removeClass(activeItemClass);
-      }
     });
 
-  $(featureListContent).children().eq(listItemIndex).css({ display: "block" });
-  $(featureListContainerSelector)
+  $(featuredContentSelector)
     .children()
     .eq(listItemIndex)
-    .addClass(activeItemClass);
+    .css({ display: "block" });
+  $(featureListSelector).children().eq(listItemIndex).addClass(activeItemClass);
 });
