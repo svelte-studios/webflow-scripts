27f717e969ae8215d742422136a7f6f1f04b0fb7
Jake Vandborg <jake.vandborg@gmail.com>
2021-08-31 12:27:09 +0930

    progress on displayOnHover

--- src/displayOnHover.js
+++ src/displayOnHover.js
@@ -4,9 +4,21 @@ const featureListContent = ".featurelistcontent";
 const contentContainerEl = document.querySelector(featureListContent);
 
 $(featureListItemSelector).hover(function () {
-  const thisIndex = $(this).index();
+  const listItemIndex = $(this).index();
+
+  $(featureListContent)
+    .children()
+    .each(function () {
+      console.log(
+        "🚀 ~ file: displayOnHover.js ~ line 17 ~ this.index()",
+        $(this).index()
+      );
+      if ($(this).css("display") !== "none") $(this).css({ display: "none" });
+    });
+
   console.log(
-    "🚀 ~ file: displayOnHover.js ~ line 12 ~ $ ~ thisIndex",
-    thisIndex
+    "🚀 ~ file: displayOnHover.js ~ line 20 ~ $(featureListContent).children().eq(listItemIndex).index()",
+    $(featureListContent).children().eq(listItemIndex).index()
   );
+  $(featureListContent).children().eq(listItemIndex).css({ display: "block" });
 });
