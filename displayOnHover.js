66dc52249f202f0770823b8f02e8ae52613a6a17
JakeVandborg <jake.vandborg@gmail.com>
2021-08-31 02:43:53 +0000

    Deploying to gh-pages from @ svelte-studios/webflow-scripts@296aa4e321ad1c6067f60248b1e84a0a7b6d64b4 🚀

--- displayOnHover.js
+++ displayOnHover.js
@@ -1,15 +1,9 @@
 "use strict";
 
-var _this = void 0;
-
 var featureListItemSelector = ".featurelistitem";
 var featureListContent = ".featurelistcontent";
 var contentContainerEl = document.querySelector(featureListContent);
 $(featureListItemSelector).hover(function () {
-  var thisIndex = $(_this).index();
-  console.log("🚀 ~ file: displayOnHover.js ~ line 12 ~ $ ~ thisIndex", thisIndex);
-});
-$(featureListItemSelector).hover(function () {
-  var thisIndex = $(_this).index();
+  var thisIndex = $(this).index();
   console.log("🚀 ~ file: displayOnHover.js ~ line 12 ~ $ ~ thisIndex", thisIndex);
 });
