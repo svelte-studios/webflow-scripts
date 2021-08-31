ba7c1fb50949fdc4a8b75285c6fc3c719ce8490e
Jake Vandborg <jake.vandborg@gmail.com>
2021-08-31 12:09:24 +0930

    lowercase classes

--- src/displayOnHover.js
+++ src/displayOnHover.js
@@ -1,7 +1,15 @@
-const featureListItemSelector = ".featureListItem";
-const featureListContent = ".featureListContent";
+const featureListItemSelector = ".featurelistitem";
+const featureListContent = ".featurelistcontent";
 
 const contentContainerEl = document.querySelector(featureListContent);
+
+$(featureListItemSelector).hover(() => {
+  const thisIndex = $(this).index();
+  console.log(
+    "🚀 ~ file: displayOnHover.js ~ line 12 ~ $ ~ thisIndex",
+    thisIndex
+  );
+});
 
 $(featureListItemSelector).hover(() => {
   const thisIndex = $(this).index();
