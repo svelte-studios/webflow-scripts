31e1b25f7d82a95e01fb43f614f52c063090a954
JakeVandborg <jake.vandborg@gmail.com>
2021-09-02 01:10:52 +0000

    Deploying to gh-pages from @ svelte-studios/webflow-scripts@9675acc72efb0b9e1f4f707005b085c541075310 🚀

--- loadMore.js
+++ loadMore.js
@@ -4,11 +4,11 @@
   var fsComponent = new FsLibrary(".load-more-list");
   fsComponent.loadmore({
     button: ".load-more-button",
-    resetIx: true,
-    animation: {
-      enable: true,
-      easing: "ease-in-out",
-      effects: "fade"
-    }
+    resetIx: true // animation: {
+    //   enable: true,
+    //   easing: "ease-in-out",
+    //   effects: "fade",
+    // },
+
   });
 })();
