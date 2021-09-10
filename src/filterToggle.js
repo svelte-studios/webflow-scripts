c2405d8dc1f8d777701c6728d21b535470640370
Ben <ben@Bens-MacBook-Pro.local>
2021-09-10 15:43:32 +0930

    loop fix

--- src/filterToggle.js
+++ src/filterToggle.js
@@ -25,8 +25,8 @@ $(filterKeySelector).each(function () {
 $(filterValueSelector).each(function () {
   const filterKeyText = convToFilter($(this).text());
   filterKeyText.split(",");
-  filterKeyText.each(function () {
-    $(this).closest(filterItemSelector).addClass(filterKeyText);
+  filterKeyText.forEach(function (i) {
+    $(this).closest(filterItemSelector).addClass(i);
   });
 });
 
