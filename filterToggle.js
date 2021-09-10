4fbae0af8d95912db571ec9f19c2ccd5a9c2f04b
BGillyVII <54088130+BGillyVII@users.noreply.github.com>
2021-09-10 04:35:46 +0000

    Deploying to gh-pages from @ svelte-studios/webflow-scripts@b5c3e28fc6fe5b1097b89db4b7232a6ed2b09527 🚀

--- filterToggle.js
+++ filterToggle.js
@@ -52,7 +52,7 @@ var toggleFilter = function toggleFilter(classVar) {
   $(filterItemSelector).each(function () {
     console.log("🚀 ~ classVar", classVar);
 
-    if ($(this).hasClass(classVar)) {
+    if ($(this).hasClass(classVar.substring(1, classVar.length))) {
       console.log("🚀 ~ file: filterToggle.js ~ line 58 ~ $(this)", $(this));
       $(this).removeClass("filter-toggle-hide");
     } else {
