abf5e8afd810e1bc21aa38b208294f4435ba25bf
BGillyVII <54088130+BGillyVII@users.noreply.github.com>
2021-11-01 02:22:36 +0000

    Deploying to gh-pages from @ svelte-studios/webflow-scripts@6b41fcc7c8b68fb30802b243ff6d6bc735f8bdf5 🚀

--- clickToggle.js
+++ clickToggle.js
@@ -1,66 +1,24 @@
 "use strict";
 
-var filterKeySelector = ".filter-toggle-key";
-var filterItemsSelector = ".filter-toggle-items";
-var filterItemSelector = ".filter-toggle-item";
-var filterValueSelector = ".filter-toggle-value";
-
-var convToFilter = function convToFilter(str) {
-  if (!str) {
-    str = "empty";
-  }
-
-  return str.replace(/[!\"#$%&'\(\)\*\+,\.\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, "").replace(/ /g, "-").toLowerCase().trim();
-};
-
-var containerEl = document.querySelector(filterItemsSelector);
-$(filterKeySelector).each(function () {
-  var filterKeyText = "." + convToFilter($(this).text());
-  $(this).attr("data-filter", filterKeyText);
+var featureListItemSelector = '.feature-click-item';
+var featuredContentSelector = '.featured-content';
+var featureListSelector = '.feature-list';
+var activeItemClass = 'active';
+$(featuredContentSelector).children().eq(0).css({
+  display: 'block'
 });
-$(filterValueSelector).each(function () {
-  var keys = $(this).text().split(",");
-  var elementToAddClass = $(this).closest(filterItemSelector);
-  keys.forEach(function (i) {
-    var filterKeyText = convToFilter(i);
-    elementToAddClass.addClass(filterKeyText);
+$(featureListItemSelector).click(function () {
+  var listItemIndex = $(this).index();
+  $(featuredContentSelector).children().each(function () {
+    if ($(this).css('display') !== 'none') $(this).css({
+      display: 'none'
+    });
   });
-});
-$(filterKeySelector).mouseenter(function () {
-  var filterValueClicked = $(this).attr("data-filter");
-  if ($(this).hasClass("active")) return; //  {
-  //   $(this).removeClass("active");
-  //   return $(filterItemSelector).removeClass("filter-toggle-hide");
-  // }
-
-  $(filterKeySelector).each(function () {
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-      toggleFilter("");
-      return;
-    }
+  $(featureListSelector).children().each(function () {
+    if ($(this).hasClass(activeItemClass)) $(this).removeClass(activeItemClass);
   });
-  toggleFilter(filterValueClicked);
-  $(this).addClass("active");
-});
-$(document).ready(function () {
-  var first = $(filterKeySelector).first();
-  var filterValueClicked = first.attr("data-filter");
-  toggleFilter(filterValueClicked);
-  first.addClass("active");
-});
-
-var toggleFilter = function toggleFilter(classVar) {
-  $(filterItemSelector).each(function () {
-    if ($(this).hasClass(classVar.substring(1, classVar.length))) {
-      $(this).removeClass("filter-toggle-hide");
-    } else {
-      $(this).addClass("filter-toggle-hide");
-    }
+  $(featuredContentSelector).children().eq(listItemIndex).css({
+    display: 'block'
   });
-};
-
-var initialFilter = function initialFilter() {
-  if (url("?filter")) return "." + url("?filter");
-  return "all";
-};
+  $(featureListSelector).children().eq(listItemIndex).addClass(activeItemClass);
+});
